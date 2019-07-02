f2fb468c5d0a93b0749357302b2c7ca610bca03f
Junwei Liang <junweil@cs.cmu.edu>
2019-07-02 10:21:54 -0700

    Update JointsDataset.py

--- lib/dataset/JointsDataset.py
+++ lib/dataset/JointsDataset.py
@@ -136,7 +136,7 @@ class JointsDataset(Dataset):
 
         if data_numpy is None:
             logger.error('=> fail to read {}'.format(image_file))
-            raise ValueError('Fail to read {}'.format(image_file))
+            #raise ValueError('Fail to read {}'.format(image_file))
 
         joints = db_rec['joints_3d']
         joints_vis = db_rec['joints_3d_vis']
